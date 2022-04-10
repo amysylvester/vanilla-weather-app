c93ac62ebae4e02ac13d5de2b41e4406e2e9f1b4
amysylvester <98198134+amysylvester@users.noreply.github.com>
2022-04-10 14:20:16 -0400

    Added search engine, js html css

--- source/function.js
+++ source/function.js
@@ -43,8 +43,19 @@ function displayTemperature(response) {
   iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
-let apiKey = "1f3e72f4f5dca90480ece5b2191a703a";
-let city = "Paris";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+function search(city) {
+  let apiKey = "1f3e72f4f5dca90480ece5b2191a703a";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayTemperature);
+}
 
-axios.get(apiUrl).then(displayTemperature);
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+}
+
+search("Paris");
+
+let form = document.querySelector("#form");
+form.addEventListener("submit", handleSubmit);
