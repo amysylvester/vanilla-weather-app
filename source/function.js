0084aba52a9449a4e48beec385dec7eeb8711476
amysylvester <98198134+amysylvester@users.noreply.github.com>
2022-04-18 20:08:46 -0400

    Troubleshooting apiKey console error

--- source/function.js
+++ source/function.js
@@ -23,6 +23,7 @@ function formatDate(timestamp) {
 }
 
 function displayForecast(response) {
+  console.log(response.data.daily);
   let forecastElement = document.querySelector("#forecast");
 
   let days = ["Thu", "Fri", "Sat", "Sun"];
@@ -37,7 +38,7 @@ function displayForecast(response) {
           class="future-weather-icons"
           src="images/weather/icons8-clouds-50.svg"
           card-img-top
-          alt="clouds"
+          alt=""
         />
         <div class="card-body">
           <h3 class="card-title"><strong>${day}</strong></h3>
@@ -56,8 +57,10 @@ function displayForecast(response) {
 }
 
 function getForecast(coordinates) {
-  let apiKey = "2cd609dbb2aa15d5aa6e9d2fe3c60fd6";
+  console.log(coordinates);
+  let apiKey = "1f3e72f4f5dca90480ece5b2191a703a";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}$unit=metric`;
+  console.log(apiUrl);
   axios.get(apiUrl).then(displayForecast);
 }
 
@@ -126,3 +129,5 @@ fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
 
 let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
+
+search("New York");
